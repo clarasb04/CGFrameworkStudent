106a006abbe883f77626e94323739d6bf097e359
clarasb04 <clara.serra04@estudiant.upf.edu>
2024-01-23 12:19:07 +0100

    triangle

--- src/framework/image.h
+++ src/framework/image.h
@@ -149,3 +149,24 @@ public:
 	Image* imatge;
 
 };
+
+class ParticleSystem {
+	static const int MAX_PARTICLES = 25;
+
+	struct Particle {
+		Vector2 position;
+		Vector2 velocity; // Normalized speed and direction of the particle
+		Color color;
+		float acceleration;
+		float ttl; // Time left until the particle expires
+		bool inactive; // Particle is not used/expired, so it can be recreated
+	};
+
+	Particle particles[MAX_PARTICLES];
+
+public:
+	void Init();
+	void Render(Image* framebuffer);
+	void Update(float dt);
+
+};
