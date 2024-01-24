e8247972787cae8460f650a2a7cdf2fdc896cdf0
Marc <marc.rodriguez09@estudiant.upf.edu>
2024-01-24 12:25:20 +0100

    butons impresos

--- src/framework/application.cpp
+++ src/framework/application.cpp
@@ -32,35 +32,54 @@ void Application::Init(void)
 
 	Image* save = new Image();
 	save->LoadPNG("images/save.png");
-	/*Image* black = new Image();
-	//black->LoadPNG("images/black.png");
+	Image* black = new Image();
+	black->LoadPNG("images/black.png");
 	Image* blue = new Image();
-	//blue->LoadPNG("images/blue.png");
+	blue->LoadPNG("images/blue.png");
 	Image* circle = new Image();
-	//circle->LoadPNG("images/circle.png");
+	circle->LoadPNG("images/circle.png");
 	Image* clear = new Image();
 	clear->LoadPNG("images/clear.png");
 	Image* cyan = new Image();
-	//cyan->LoadPNG("images/cyan.png");
+	cyan->LoadPNG("images/cyan.png");
 	Image* eraser = new Image();
-	//eraser->LoadPNG("images/eraser.png");
+	eraser->LoadPNG("images/eraser.png");
 	Image* green = new Image();
-	//green->LoadPNG("images/green.png");
+	green->LoadPNG("images/green.png");
 	Image* line = new Image();
-	//line->LoadPNG("images/line.png");
+	line->LoadPNG("images/line.png");
 	Image* load = new Image();
 	load->LoadPNG("images/load.png");
 	Image* pink = new Image();
-	//pink->LoadPNG("images/pink.png");
+	pink->LoadPNG("images/pink.png");
 	Image* rect = new Image();
-	//rect->LoadPNG("images/rectangle.png");
+	rect->LoadPNG("images/rectangle.png");
 	Image* red = new Image();
-	//red->LoadPNG("images/red.png");
+	red->LoadPNG("images/red.png");
+	Image* white = new Image();
+	white->LoadPNG("images/white.png");
+	Image* triang = new Image();
+	triang->LoadPNG("images/triangle.png");
+	Image* yell = new Image();
+	yell->LoadPNG("images/yellow.png");
 
 
 	b_clear =  Button(clear, 10, 10);
 	b_load = Button(load, 52, 10);
-	*/
+	b_save = Button(save, 94, 10);
+	b_eraser = Button(eraser, 136, 10);
+	b_line = Button(line, 178, 10);
+	b_rect = Button(rect, 220, 10);
+	b_circle = Button(circle, 262, 10);
+	b_triang = Button(triang, 304, 10);
+	b_black = Button(black, 346, 10);
+	b_white = Button(white, 388, 10);
+	b_pink = Button(pink, 430, 10);
+	b_yell = Button(yell, 472, 10);
+	b_green = Button(green, 514, 10);
+	b_red = Button(red, 556, 10);
+	b_blue = Button(blue, 598, 10);
+	b_cyan = Button(cyan, 640, 10);
 	
 
 }
@@ -84,8 +103,26 @@ void Application::Render(void)
 	framebuffer.Render();
 	*/
 	//particleSystem.Render(&framebuffer);
+
+	framebuffer.DrawRect(0, 0,1280, 52, Color::GRAY, 1, TRUE, Color::GRAY);
 	framebuffer.DrawImage(*b_save.imatge, b_save.x, b_save.y, FALSE);
-	//framebuffer.DrawImage(*b_load.imatge, b_load.x, b_load.y, FALSE);
+	framebuffer.DrawImage(*b_load.imatge, b_load.x, b_load.y, FALSE);
+	framebuffer.DrawImage(*b_clear.imatge, b_clear.x, b_clear.y, FALSE);
+	framebuffer.DrawImage(*b_eraser.imatge, b_eraser.x, b_eraser.y, FALSE);
+	framebuffer.DrawImage(*b_line.imatge, b_line.x, b_line.y, FALSE);
+	framebuffer.DrawImage(*b_rect.imatge, b_rect.x, b_rect.y, FALSE);
+	framebuffer.DrawImage(*b_circle.imatge, b_circle.x, b_circle.y, FALSE);
+	framebuffer.DrawImage(*b_triang.imatge, b_triang.x, b_triang.y, FALSE);
+	framebuffer.DrawImage(*b_black.imatge, b_black.x, b_black.y, FALSE);
+	framebuffer.DrawImage(*b_white.imatge, b_white.x, b_white.y, FALSE);
+	framebuffer.DrawImage(*b_pink.imatge, b_pink.x, b_pink.y, FALSE);
+	framebuffer.DrawImage(*b_yell.imatge, b_yell.x, b_yell.y, FALSE);
+	framebuffer.DrawImage(*b_green.imatge, b_green.x, b_green.y, FALSE);
+	framebuffer.DrawImage(*b_red.imatge, b_red.x, b_red.y, FALSE);
+	framebuffer.DrawImage(*b_blue.imatge, b_blue.x, b_blue.y, FALSE);
+	framebuffer.DrawImage(*b_cyan.imatge, b_cyan.x, b_cyan.y, FALSE);
+
+
 
 
 
@@ -125,7 +162,12 @@ void Application::OnKeyPressed( SDL_KeyboardEvent event )
 void Application::OnMouseButtonDown( SDL_MouseButtonEvent event )
 {
 	if (event.button == SDL_BUTTON_LEFT) {
+		if (b_clear.IsMouseInside(mouse_position)) {
 
+		}
+		if(b_load.IsMouseInside(mouse_position)){
+
+		}
 	}
 }
 
